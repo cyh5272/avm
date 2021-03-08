5d4573e8a11d95cfc223c7b40bcf033c33ec3a70
Urvang Joshi <urvang@google.com>
2021-03-08 14:52:53 -0800

    Add 2 CNN restoration tools.
    Moved two tools from experimental branch:
    - CONFIG_LOOP_RESTORE_CNN
    - CONFIG_CNN_RESTORATION
    
    Note: Also includes the option CONFIG_CNN_RESTORATION_SMALL_MODELS that
    uses smaller models in combination with one of the two tools above.
    
    Change-Id: I135e8c425bb916d42a638adf3a6bd23cf3f5bf52

--- av1/common/av1_common_int.h
+++ av1/common/av1_common_int.h
@@ -1492,6 +1492,16 @@ typedef struct AV1Common {
    */
   TIP tip_ref;
 #endif  // CONFIG_TIP
+#if CONFIG_CNN_RESTORATION || CONFIG_LOOP_RESTORE_CNN
+  /*!
+   * True if luma channel is using CNN restoration.
+   */
+  int use_cnn_y;
+  /*!
+   * True if chroma channels are using CNN restoration.
+   */
+  int use_cnn_uv;
+#endif  // CONFIG_CNN_RESTORATION || CONFIG_LOOP_RESTORE_CNN
 } AV1_COMMON;
 
 /*!\cond */
