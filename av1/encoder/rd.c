34b292c3b0d98540c57c4838d5aaa9c858d39f22
oguleryuz <oguleryuz@google.com>
2021-05-10 17:02:51 -0700

    Continues adding nonseparable Wiener filter changes.
    Change-Id: Ibb8cab13c7c3c099759046661e6482eb1322a8fa

--- av1/encoder/rd.c
+++ av1/encoder/rd.c
@@ -289,6 +289,11 @@ void av1_fill_mode_rates(AV1_COMMON *const cm, const MACROBLOCKD *xd,
   }
 
 #endif  // !CONFIG_AIMC
+
+#if CONFIG_WIENER_NONSEP
+  av1_cost_tokens_from_cdf(mode_costs->wiener_nonsep_restore_cost,
+                           fc->wiener_nonsep_restore_cdf, NULL);
+#endif  // CONFIG_WIENER_NONSEP
   av1_cost_tokens_from_cdf(mode_costs->intrabc_cost, fc->intrabc_cdf, NULL);
 #if CONFIG_BVP_IMPROVEMENT
   av1_cost_tokens_from_cdf(mode_costs->intrabc_mode_cost, fc->intrabc_mode_cdf,
