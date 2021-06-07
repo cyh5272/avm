11d5598d2857c45ea8dc7634ad6f12635da7f994
oguleryuz <oguleryuz@google.com>
2021-06-07 11:30:08 -0700

    Adds support for CONFIG_RST_MERGECOEFFS.
    Change-Id: I6a24d181dc5c977841440e14b684efb35d41cde0

--- av1/encoder/bitstream.c
+++ av1/encoder/bitstream.c
@@ -2519,10 +2519,19 @@ static AOM_INLINE void encode_restoration_mode(
   }
 }
 
-static AOM_INLINE void write_wiener_filter(int wiener_win,
+static AOM_INLINE void write_wiener_filter(MACROBLOCKD *xd, int wiener_win,
                                            const WienerInfo *wiener_info,
                                            WienerInfo *ref_wiener_info,
                                            aom_writer *wb) {
+#if CONFIG_RST_MERGECOEFFS
+  const int equal = check_wiener_eq(wiener_info, ref_wiener_info);
+  aom_write_symbol(wb, equal, xd->tile_ctx->merged_param_cdf, 2);
+  if (equal) {
+    return;
+  }
+#else
+  (void)xd;
+#endif  // CONFIG_RST_MERGECOEFFS
   if (wiener_win == WIENER_WIN)
     aom_write_primitive_refsubexpfin(
         wb, WIENER_FILT_TAP0_MAXV - WIENER_FILT_TAP0_MINV + 1,
@@ -2564,9 +2573,20 @@ static AOM_INLINE void write_wiener_filter(int wiener_win,
   memcpy(ref_wiener_info, wiener_info, sizeof(*wiener_info));
 }
 
-static AOM_INLINE void write_sgrproj_filter(const SgrprojInfo *sgrproj_info,
+static AOM_INLINE void write_sgrproj_filter(MACROBLOCKD *xd,
+                                            const SgrprojInfo *sgrproj_info,
                                             SgrprojInfo *ref_sgrproj_info,
                                             aom_writer *wb) {
+#if CONFIG_RST_MERGECOEFFS
+  const int equal = check_sgrproj_eq(sgrproj_info, ref_sgrproj_info);
+  aom_write_symbol(wb, equal, xd->tile_ctx->merged_param_cdf, 2);
+  if (equal) {
+    memcpy(ref_sgrproj_info, sgrproj_info, sizeof(*sgrproj_info));
+    return;
+  }
+#else
+  (void)xd;
+#endif  // CONFIG_RST_MERGECOEFFS
   aom_write_literal(wb, sgrproj_info->ep, SGRPROJ_PARAMS_BITS);
   const sgr_params_type *params = &av1_sgr_params[sgrproj_info->ep];
 
@@ -2600,7 +2620,16 @@ static void write_wiener_nsfilter(MACROBLOCKD *xd, int is_uv,
                                   const WienerNonsepInfo *wienerns_info,
                                   WienerNonsepInfo *ref_wienerns_info,
                                   aom_writer *wb) {
+#if CONFIG_RST_MERGECOEFFS
+  const int equal = check_wienerns_eq(is_uv, wienerns_info, ref_wienerns_info);
+  aom_write_symbol(wb, equal, xd->tile_ctx->merged_param_cdf, 2);
+  if (equal) {
+    memcpy(ref_wienerns_info, wienerns_info, sizeof(*wienerns_info));
+    return;
+  }
+#else
   (void)xd;
+#endif  // CONFIG_RST_MERGECOEFFS
   int beg_feat = is_uv ? wienerns_y : 0;
   int end_feat = is_uv ? wienerns_y + wienerns_uv : wienerns_y;
   const int(*wienerns_coeffs)[3] = is_uv ? wienerns_coeff_uv : wienerns_coeff_y;
@@ -2662,10 +2691,11 @@ static AOM_INLINE void loop_restoration_write_sb_coeffs(
 
     switch (unit_rtype) {
       case RESTORE_WIENER:
-        write_wiener_filter(wiener_win, &rui->wiener_info, ref_wiener_info, w);
+        write_wiener_filter(xd, wiener_win, &rui->wiener_info, ref_wiener_info,
+                            w);
         break;
       case RESTORE_SGRPROJ:
-        write_sgrproj_filter(&rui->sgrproj_info, ref_sgrproj_info, w);
+        write_sgrproj_filter(xd, &rui->sgrproj_info, ref_sgrproj_info, w);
         break;
 #if CONFIG_LOOP_RESTORE_CNN
       case RESTORE_CNN: break;
@@ -2685,7 +2715,8 @@ static AOM_INLINE void loop_restoration_write_sb_coeffs(
     ++counts->wiener_restore[unit_rtype != RESTORE_NONE];
 #endif
     if (unit_rtype != RESTORE_NONE) {
-      write_wiener_filter(wiener_win, &rui->wiener_info, ref_wiener_info, w);
+      write_wiener_filter(xd, wiener_win, &rui->wiener_info, ref_wiener_info,
+                          w);
     }
   } else if (frame_rtype == RESTORE_SGRPROJ) {
     aom_write_symbol(w, unit_rtype != RESTORE_NONE,
@@ -2694,7 +2725,7 @@ static AOM_INLINE void loop_restoration_write_sb_coeffs(
     ++counts->sgrproj_restore[unit_rtype != RESTORE_NONE];
 #endif
     if (unit_rtype != RESTORE_NONE) {
-      write_sgrproj_filter(&rui->sgrproj_info, ref_sgrproj_info, w);
+      write_sgrproj_filter(xd, &rui->sgrproj_info, ref_sgrproj_info, w);
     }
 #if CONFIG_LOOP_RESTORE_CNN
   } else if (frame_rtype == RESTORE_CNN) {
