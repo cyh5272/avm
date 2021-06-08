644986c08ed64b7154ea9dec7a9ca4e191d927ce
Urvang Joshi <urvang@google.com>
2021-06-08 12:24:53 -0700

    Remove CONFIG_LOOP_RESTORE_CNN.
    The other alternate CONFIG_CNN_RESTORATION seems to be preferred as it
    uses CNN at the frame level.
    
    Change-Id: Ia7a46ac3eb5a40f3f3e52f7b6ea6c19de5b11a2a

--- av1/common/av1_common_int.h
+++ av1/common/av1_common_int.h
@@ -1495,7 +1495,7 @@ typedef struct AV1Common {
    */
   TIP tip_ref;
 #endif  // CONFIG_TIP
-#if CONFIG_CNN_RESTORATION || CONFIG_LOOP_RESTORE_CNN
+#if CONFIG_CNN_RESTORATION
   /*!
    * True if luma channel is using CNN restoration.
    */
@@ -1504,7 +1504,7 @@ typedef struct AV1Common {
    * True if chroma channels are using CNN restoration.
    */
   int use_cnn_uv;
-#endif  // CONFIG_CNN_RESTORATION || CONFIG_LOOP_RESTORE_CNN
+#endif  // CONFIG_CNN_RESTORATION
 } AV1_COMMON;
 
 /*!\cond */
