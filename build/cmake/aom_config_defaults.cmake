d7fcbc4b3689bac9ae96860fe1b98be745170dc0
Mei Guo <mei.guo@apple.com>
2021-10-08 14:28:18 -0700

    Turn on compiler flags for IBP

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -171,9 +171,9 @@ set_aom_config_var(CONFIG_QBASED_QP_OFFSET 1
                    "AV2 experiment flag to adjust q_offset based on QP.")
 set_aom_config_var(CONFIG_OPTFLOW_REFINEMENT 1
                    "AV2 experiment flag for optical flow MV refinement")
-set_aom_config_var(CONFIG_IBP_DIR 0
+set_aom_config_var(CONFIG_IBP_DIR 1
                    "AV2 experiment flag to enable intra bi-prediction for directional modes.")
-set_aom_config_var(CONFIG_IBP_DC 0
+set_aom_config_var(CONFIG_IBP_DC 1
                    "AV2 experiment flag to enable intra bi-prediction for DC mode.")
 
 #
