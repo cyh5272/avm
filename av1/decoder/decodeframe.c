b94857af858768a76f3bb1cb2658b53a3d8fe4c6
Debargha Mukherjee <debargha@google.com>
2022-01-27 20:14:40 +0000

    Setup buffers and defaults correctly for superres

--- av1/decoder/decodeframe.c
+++ av1/decoder/decodeframe.c
@@ -2835,6 +2835,11 @@ static AOM_INLINE void setup_superres(AV1_COMMON *const cm,
                                       int *width, int *height) {
   cm->superres_upscaled_width = *width;
   cm->superres_upscaled_height = *height;
+  // Set default to 1:1 scaling - ie. no scaling, scale not provided
+  cm->superres_scale_denominator = SCALE_NUMERATOR;
+#if CONFIG_EXT_SUPERRES
+  cm->superres_scale_numerator = SCALE_NUMERATOR;
+#endif  // CONFIG_EXT_SUPERRES
 
   const SequenceHeader *const seq_params = &cm->seq_params;
   if (!seq_params->enable_superres) return;
@@ -2858,12 +2863,6 @@ static AOM_INLINE void setup_superres(AV1_COMMON *const cm,
     // resized correctly
     av1_calculate_scaled_superres_size(width, height,
                                        cm->superres_scale_denominator);
-#endif  // CONFIG_EXT_SUPERRES
-  } else {
-    // 1:1 scaling - ie. no scaling, scale not provided
-    cm->superres_scale_denominator = SCALE_NUMERATOR;
-#if CONFIG_EXT_SUPERRES
-    cm->superres_scale_numerator = SCALE_NUMERATOR;
 #endif  // CONFIG_EXT_SUPERRES
   }
 }
