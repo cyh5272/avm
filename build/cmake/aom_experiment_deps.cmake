de8dae4e37e74a8c7d143d9345be292426b13dc2
Mohammed Golam Sarwer <msarwer@google.com>
2022-02-28 21:44:56 +0000

    Restrict AMVR and AMVD enable at same time
    This commit restrict to enabling both CONFIG_ADAPTIVE_MVD and CONFIG_FLEX_MVRES at same time.
    STATS_CHANGED

--- build/cmake/aom_experiment_deps.cmake
+++ build/cmake/aom_experiment_deps.cmake
@@ -49,4 +49,8 @@ macro(fix_experiment_configs)
     change_config_and_warn(CONFIG_CCSO_EXT 0 !CONFIG_CCSO)
   endif()
 
+  if(CONFIG_FLEX_MVRES AND CONFIG_ADAPTIVE_MVD)
+    change_config_and_warn(CONFIG_FLEX_MVRES 0 CONFIG_ADAPTIVE_MVD)
+  endif()
+
 endmacro()
