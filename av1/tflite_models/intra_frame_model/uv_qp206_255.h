0206fa4cae14f58a3b340c546bf37166c627e743
Urvang Joshi <urvang@google.com>
2022-03-05 01:45:02 +0000

    CNN Restoration: Updated chroma models.

--- av1/tflite_models/intra_frame_model/uv_qp206_255.h
+++ av1/tflite_models/intra_frame_model/uv_qp206_255.h
@@ -9,9 +9,9 @@
  * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
  */
 
-#ifndef AOM_AV1_TFLITE_MODELS_INTRA_FRAME_MODEL_UV_QP32_H_
-#define AOM_AV1_TFLITE_MODELS_INTRA_FRAME_MODEL_UV_QP32_H_
+#ifndef AOM_AV1_TFLITE_MODELS_INTRA_FRAME_MODEL_UV_QP206_255_H_
+#define AOM_AV1_TFLITE_MODELS_INTRA_FRAME_MODEL_UV_QP206_255_H_
 
-extern const unsigned char uv_qp32_model_tflite_data[];
+extern const unsigned char uv_qp206_255_model_tflite_data[];
 
-#endif  // AOM_AV1_TFLITE_MODELS_INTRA_FRAME_MODEL_UV_QP32_H_
+#endif  // AOM_AV1_TFLITE_MODELS_INTRA_FRAME_MODEL_UV_QP206_255_H_
