wjdz99/aom

Continues adding nonseparable Wiener filter changes.
Change-Id: Ibb8cab13c7c3c099759046661e6482eb1322a8fa

av1/encoder/rd.c
@@ -335,6 +335,10 @@ void av1_fill_lr_rates(ModeCosts *mode_costs, FRAME_CONTEXT *fc) {
   av1_cost_tokens_from_cdf(mode_costs->cnn_restore_cost, fc->cnn_restore_cdf,
                            NULL);
 #endif  // CONFIG_LOOP_RESTORE_CNN
+#if CONFIG_WIENER_NONSEP
+  av1_cost_tokens_from_cdf(x->wiener_nonsep_restore_cost,
+                           fc->wiener_nonsep_restore_cdf, NULL);
+#endif  // CONFIG_WIENER_NONSEP
 }
 
 // Values are now correlated to quantizer.

cyh5272/avm

av1/encoder/rd.c
@@ -286,6 +286,11 @@ void av1_fill_mode_rates(AV1_COMMON *const cm, const MACROBLOCKD *xd,
   }
 
 #endif  // !CONFIG_AIMC
+
+#if CONFIG_WIENER_NONSEP
+  av1_cost_tokens_from_cdf(mode_costs->wiener_nonsep_restore_cost,
+                           fc->wiener_nonsep_restore_cdf, NULL);
+#endif  // CONFIG_WIENER_NONSEP
   av1_cost_tokens_from_cdf(mode_costs->intrabc_cost, fc->intrabc_cdf, NULL);
 
 #if CONFIG_IST