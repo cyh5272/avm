594e5c2019cf17d59731f7d33e0b8b1b4e434d5a
Mohammed Golam Sarwer <msarwer@google.com>
2022-04-27 23:47:37 +0000

    Bug fix of amvr
    In this commit, a bug is fixed related to interaction between amvr and motion
    modes.
    
    STATS_CHANGED

--- av1/common/blockd.c
+++ av1/common/blockd.c
@@ -170,8 +170,7 @@ motion_mode_allowed(const WarpedMotionParams *gm_params, const MACROBLOCKD *xd,
   }
 
 #if CONFIG_FLEX_MVRES && DISABLE_OBMC_WARPED_INTER_INTRA_LOWER_PRECISION
-  if (mbmi->pb_mv_precision <= mbmi->max_mv_precision)
-    return SIMPLE_TRANSLATION;
+  if (mbmi->pb_mv_precision < mbmi->max_mv_precision) return SIMPLE_TRANSLATION;
 #endif
   if (is_motion_variation_allowed_bsize(mbmi->sb_type[PLANE_TYPE_Y]) &&
       is_inter_mode(mbmi->mode) && mbmi->ref_frame[1] != INTRA_FRAME &&
