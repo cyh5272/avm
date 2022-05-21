b4e424f4a2ac4e873470f76d9d4579f340c202a4
Cosmin Stejerean <cstejerean@fb.com>
2022-05-21 12:19:42 -0700

    update message for disabling runtime cpu detection

--- aom_ports/arm_cpudetect.c
+++ aom_ports/arm_cpudetect.c
@@ -146,6 +146,6 @@ int aom_arm_cpu_caps(void) {
 }
 #else  /* end __linux__ */
 #error \
-    "--enable-runtime-cpu-detect selected, but no CPU detection method " \
-"available for your platform. Reconfigure with --disable-runtime-cpu-detect."
+    "CONFIG_RUNTIME_CPU_DETECT=1 selected, but no CPU detection method " \
+"available for your platform. Reconfigure with CONFIG_RUNTIME_CPU_DETECT=0"
 #endif
