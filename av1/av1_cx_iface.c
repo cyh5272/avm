cyh5272/avm

commit 857e93fa9519ec2478eacd44f32a40ac05535b23

Remove disabled low bit-depth codepath
This MR attempts to remove the low bitdepth codepath and the public API related to it.

AOM_IMG_FMT_HIGHBITDEPTH is left in place in order to let the library users feed the encoder frames using 8bit buffers for 8bit content. These are in turn upshifted to 16bit buffers in the aom_codec_encode_fn_t callback from aom_codec_av1_cx_algo to be handled internally (the aomenc CLI is already doing the same, but other library users do not benefit from it).

av1/av1_cx_iface.c
@@ -22,6 +22,7 @@
 
 #include "aom/aom_encoder.h"
 #include "aom/internal/aom_codec_internal.h"
+#include "aom/internal/aom_image_internal.h"
 
 #include "av1/av1_iface_common.h"
 #include "av1/encoder/bitstream.h"
@@ -2548,8 +2549,6 @@ static aom_codec_err_t encoder_init(aom_codec_ctx_t *ctx) {
           lap_lag_in_frames = LAP_LAG_IN_FRAMES;
         }
       }
-      priv->oxcf.use_highbitdepth =
-          (ctx->init_flags & AOM_CODEC_USE_HIGHBITDEPTH) ? 1 : 0;
 
       res = create_stats_buffer(&priv->frame_stats_buffer,
                                 &priv->stats_buf_context, *num_lap_buffers);
@@ -2811,14 +2810,22 @@ static aom_codec_err_t encoder_encode(aom_codec_alg_priv_t *ctx,
           timebase_units_to_ticks(timestamp_ratio, ptsvol + duration);
 
       YV12_BUFFER_CONFIG sd;
-      res = image2yuvconfig(img, &sd);
+      aom_image_t *hbd_img = NULL;
+      // May need to allocate larger buffer to use hbd internal.
+      if (!(img->fmt & AOM_IMG_FMT_HIGHBITDEPTH)) {
+        hbd_img = aom_img_alloc(NULL, img->fmt | AOM_IMG_FMT_HIGHBITDEPTH,
+                                img->w, img->h, 32);
+        if (!hbd_img) return AOM_CODEC_MEM_ERROR;
+        image2yuvconfig_upshift(hbd_img, img, &sd);
+      } else {
+        res = image2yuvconfig(img, &sd);
+      }
       // When generating a monochrome stream, make |sd| a monochrome image.
       if (ctx->cfg.monochrome) {
         sd.u_buffer = sd.v_buffer = NULL;
         sd.uv_stride = 0;
         sd.monochrome = 1;
       }
-      int use_highbitdepth = (sd.flags & YV12_FLAG_HIGHBITDEPTH) != 0;
       int subsampling_x = sd.subsampling_x;
       int subsampling_y = sd.subsampling_y;
 
@@ -2828,7 +2835,7 @@ static aom_codec_err_t encoder_encode(aom_codec_alg_priv_t *ctx,
 
         cpi->lookahead = av1_lookahead_init(
             cpi->oxcf.frm_dim_cfg.width, cpi->oxcf.frm_dim_cfg.height,
-            subsampling_x, subsampling_y, use_highbitdepth, lag_in_frames,
+            subsampling_x, subsampling_y, lag_in_frames,
             cpi->oxcf.border_in_pixels, cpi->common.features.byte_alignment,
             ctx->num_lap_buffers);
       }
@@ -2836,12 +2843,10 @@ static aom_codec_err_t encoder_encode(aom_codec_alg_priv_t *ctx,
         aom_internal_error(&cpi->common.error, AOM_CODEC_MEM_ERROR,
                            "Failed to allocate lag buffers");
 
-      av1_check_initial_width(cpi, use_highbitdepth, subsampling_x,
-                              subsampling_y);
+      av1_check_initial_width(cpi, subsampling_x, subsampling_y);
       if (cpi_lap != NULL) {
         cpi_lap->lookahead = cpi->lookahead;
-        av1_check_initial_width(cpi_lap, use_highbitdepth, subsampling_x,
-                                subsampling_y);
+        av1_check_initial_width(cpi_lap, subsampling_x, subsampling_y);
       }
 
       // Store the original flags in to the frame buffer. Will extract the
@@ -2850,6 +2855,7 @@ static aom_codec_err_t encoder_encode(aom_codec_alg_priv_t *ctx,
                                 src_time_stamp, src_end_time_stamp)) {
         res = update_error_state(ctx, &cpi->common.error);
       }
+      aom_img_free(hbd_img);
       ctx->next_frame_flags = 0;
     }
 
@@ -3069,10 +3075,19 @@ static aom_codec_err_t ctrl_set_reference(aom_codec_alg_priv_t *ctx,
   av1_ref_frame_t *const frame = va_arg(args, av1_ref_frame_t *);
 
   if (frame != NULL) {
+    aom_image_t *hbd_img = NULL;
     YV12_BUFFER_CONFIG sd;
 
-    image2yuvconfig(&frame->img, &sd);
+    if (!(frame->img.fmt & AOM_IMG_FMT_HIGHBITDEPTH)) {
+      hbd_img = aom_img_alloc(NULL, frame->img.fmt | AOM_IMG_FMT_HIGHBITDEPTH,
+                              frame->img.w, frame->img.h, 32);
+      if (!hbd_img) return AOM_CODEC_MEM_ERROR;
+      image2yuvconfig_upshift(hbd_img, &frame->img, &sd);
+    } else {
+      image2yuvconfig(&frame->img, &sd);
+    }
     av1_set_reference_enc(ctx->cpi, frame->idx, &sd);
+    aom_img_free(hbd_img);
     return AOM_CODEC_OK;
   } else {
     return AOM_CODEC_INVALID_PARAM;
@@ -3084,10 +3099,19 @@ static aom_codec_err_t ctrl_copy_reference(aom_codec_alg_priv_t *ctx,
   av1_ref_frame_t *const frame = va_arg(args, av1_ref_frame_t *);
 
   if (frame != NULL) {
+    aom_image_t *hbd_img = NULL;
     YV12_BUFFER_CONFIG sd;
 
-    image2yuvconfig(&frame->img, &sd);
+    if (!(frame->img.fmt & AOM_IMG_FMT_HIGHBITDEPTH)) {
+      hbd_img = aom_img_alloc(NULL, frame->img.fmt | AOM_IMG_FMT_HIGHBITDEPTH,
+                              frame->img.w, frame->img.h, 32);
+      if (!hbd_img) return AOM_CODEC_MEM_ERROR;
+      image2yuvconfig_upshift(hbd_img, &frame->img, &sd);
+    } else {
+      image2yuvconfig(&frame->img, &sd);
+    }
     av1_copy_reference_enc(ctx->cpi, frame->idx, &sd);
+    aom_img_free(hbd_img);
     return AOM_CODEC_OK;
   } else {
     return AOM_CODEC_INVALID_PARAM;
@@ -3135,9 +3159,21 @@ static aom_codec_err_t ctrl_copy_new_frame_image(aom_codec_alg_priv_t *ctx,
     YV12_BUFFER_CONFIG new_frame;
 
     if (av1_get_last_show_frame(ctx->cpi, &new_frame) == 0) {
+      aom_image_t *hbd_img = NULL;
       YV12_BUFFER_CONFIG sd;
-      image2yuvconfig(new_img, &sd);
-      return av1_copy_new_frame_enc(&ctx->cpi->common, &new_frame, &sd);
+
+      if (!(new_img->fmt & AOM_IMG_FMT_HIGHBITDEPTH)) {
+        hbd_img = aom_img_alloc(NULL, new_img->fmt | AOM_IMG_FMT_HIGHBITDEPTH,
+                                new_img->w, new_img->h, 32);
+        if (!hbd_img) return AOM_CODEC_MEM_ERROR;
+        image2yuvconfig_upshift(hbd_img, new_img, &sd);
+      } else {
+        image2yuvconfig(new_img, &sd);
+      }
+      aom_codec_err_t res =
+          av1_copy_new_frame_enc(&ctx->cpi->common, &new_frame, &sd);
+      aom_img_free(hbd_img);
+      return res;
     } else {
       return AOM_CODEC_ERROR;
     }
@@ -4047,11 +4083,10 @@ static const aom_codec_enc_cfg_t encoder_usage_cfg[] = { {
 aom_codec_iface_t aom_codec_av1_cx_algo = {
   "AOMedia Project AV1 Encoder" VERSION_STRING,
   AOM_CODEC_INTERNAL_ABI_VERSION,
-  AOM_CODEC_CAP_HIGHBITDEPTH | AOM_CODEC_CAP_ENCODER |
-      AOM_CODEC_CAP_PSNR,  // aom_codec_caps_t
-  encoder_init,            // aom_codec_init_fn_t
-  encoder_destroy,         // aom_codec_destroy_fn_t
-  encoder_ctrl_maps,       // aom_codec_ctrl_fn_map_t
+  AOM_CODEC_CAP_ENCODER | AOM_CODEC_CAP_PSNR,  // aom_codec_caps_t
+  encoder_init,                                // aom_codec_init_fn_t
+  encoder_destroy,                             // aom_codec_destroy_fn_t
+  encoder_ctrl_maps,                           // aom_codec_ctrl_fn_map_t
   {
       // NOLINT
       NULL,  // aom_codec_peek_si_fn_t

av1/av1_dx_iface.c
@@ -93,8 +93,6 @@ static aom_codec_err_t decoder_init(aom_codec_ctx_t *ctx) {
     ctx->priv->init_flags = ctx->init_flags;
     priv->flushed = 0;
 
-    // TODO(tdaede): this should not be exposed to the API
-    priv->cfg.allow_lowbitdepth = 0;
     if (ctx->config.dec) {
       priv->cfg = *ctx->config.dec;
       ctx->config.dec = &priv->cfg;
@@ -470,7 +468,6 @@ static aom_codec_err_t init_decoder(aom_codec_alg_priv_t *ctx) {
   }
   frame_worker_data->frame_context_ready = 0;
   frame_worker_data->received_frame = 0;
-  frame_worker_data->pbi->allow_lowbitdepth = ctx->cfg.allow_lowbitdepth;
 
   // If decoding in serial mode, FrameWorker thread could create tile worker
   // thread or loopfilter thread.
@@ -825,13 +822,11 @@ static aom_image_t *decoder_get_frame(aom_codec_alg_priv_t *ctx,
           const int tile_col = AOMMIN(pbi->dec_tile_col, tiles->cols - 1);
           const int mi_col = tile_col * tile_width;
           const int ssx = ctx->img.x_chroma_shift;
-          const int is_hbd = (ctx->img.fmt & AOM_IMG_FMT_HIGHBITDEPTH) ? 1 : 0;
           int plane;
-          ctx->img.planes[0] += mi_col * MI_SIZE * (1 + is_hbd);
+          ctx->img.planes[0] += mi_col * MI_SIZE * 2;
           if (num_planes > 1) {
             for (plane = 1; plane < MAX_MB_PLANE; ++plane) {
-              ctx->img.planes[plane] +=
-                  mi_col * (MI_SIZE >> ssx) * (1 + is_hbd);
+              ctx->img.planes[plane] += mi_col * (MI_SIZE >> ssx) * 2;
             }
           }
           ctx->img.d_w =
@@ -884,13 +879,25 @@ static aom_codec_err_t ctrl_set_reference(aom_codec_alg_priv_t *ctx,
   av1_ref_frame_t *const data = va_arg(args, av1_ref_frame_t *);
 
   if (data) {
+    aom_image_t *hbd_img = NULL;
     av1_ref_frame_t *const frame = data;
     YV12_BUFFER_CONFIG sd;
     AVxWorker *const worker = ctx->frame_worker;
     FrameWorkerData *const frame_worker_data = (FrameWorkerData *)worker->data1;
-    image2yuvconfig(&frame->img, &sd);
-    return av1_set_reference_dec(&frame_worker_data->pbi->common, frame->idx,
-                                 frame->use_external_ref, &sd);
+    if (!(frame->img.fmt & AOM_IMG_FMT_HIGHBITDEPTH)) {
+      if (frame->use_external_ref) return AOM_CODEC_INVALID_PARAM;
+      hbd_img = aom_img_alloc(NULL, frame->img.fmt | AOM_IMG_FMT_HIGHBITDEPTH,
+                              frame->img.w, frame->img.h, 32);
+      if (!hbd_img) return AOM_CODEC_MEM_ERROR;
+      image2yuvconfig_upshift(hbd_img, &frame->img, &sd);
+    } else {
+      image2yuvconfig(&frame->img, &sd);
+    }
+    aom_codec_err_t res =
+        av1_set_reference_dec(&frame_worker_data->pbi->common, frame->idx,
+                              frame->use_external_ref, &sd);
+    aom_img_free(hbd_img);
+    return res;
   } else {
     return AOM_CODEC_INVALID_PARAM;
   }
@@ -900,11 +907,22 @@ static aom_codec_err_t ctrl_copy_reference(aom_codec_alg_priv_t *ctx,
                                            va_list args) {
   const av1_ref_frame_t *const frame = va_arg(args, av1_ref_frame_t *);
   if (frame) {
+    aom_image_t *hbd_img = NULL;
     YV12_BUFFER_CONFIG sd;
     AVxWorker *const worker = ctx->frame_worker;
     FrameWorkerData *const frame_worker_data = (FrameWorkerData *)worker->data1;
-    image2yuvconfig(&frame->img, &sd);
-    return av1_copy_reference_dec(frame_worker_data->pbi, frame->idx, &sd);
+    if (!(frame->img.fmt & AOM_IMG_FMT_HIGHBITDEPTH)) {
+      hbd_img = aom_img_alloc(NULL, frame->img.fmt | AOM_IMG_FMT_HIGHBITDEPTH,
+                              frame->img.w, frame->img.h, 32);
+      if (!hbd_img) return AOM_CODEC_MEM_ERROR;
+      image2yuvconfig_upshift(hbd_img, &frame->img, &sd);
+    } else {
+      image2yuvconfig(&frame->img, &sd);
+    }
+    aom_codec_err_t res =
+        av1_copy_reference_dec(frame_worker_data->pbi, frame->idx, &sd);
+    aom_img_free(hbd_img);
+    return res;
   } else {
     return AOM_CODEC_INVALID_PARAM;
   }
@@ -954,10 +972,20 @@ static aom_codec_err_t ctrl_copy_new_frame_image(aom_codec_alg_priv_t *ctx,
     FrameWorkerData *const frame_worker_data = (FrameWorkerData *)worker->data1;
 
     if (av1_get_frame_to_show(frame_worker_data->pbi, &new_frame) == 0) {
+      aom_image_t *hbd_img = NULL;
       YV12_BUFFER_CONFIG sd;
-      image2yuvconfig(img, &sd);
-      return av1_copy_new_frame_dec(&frame_worker_data->pbi->common, &new_frame,
-                                    &sd);
+      if (!(img->fmt & AOM_IMG_FMT_HIGHBITDEPTH)) {
+        hbd_img = aom_img_alloc(NULL, img->fmt | AOM_IMG_FMT_HIGHBITDEPTH,
+                                img->w, img->h, 32);
+        if (!hbd_img) return AOM_CODEC_MEM_ERROR;
+        image2yuvconfig_upshift(hbd_img, img, &sd);
+      } else {
+        image2yuvconfig(img, &sd);
+      }
+      aom_codec_err_t res = av1_copy_new_frame_dec(
+          &frame_worker_data->pbi->common, &new_frame, &sd);
+      aom_img_free(hbd_img);
+      return res;
     } else {
       return AOM_CODEC_ERROR;
     }
@@ -1357,8 +1385,7 @@ static aom_codec_err_t ctrl_get_bit_depth(aom_codec_alg_priv_t *ctx,
   return AOM_CODEC_INVALID_PARAM;
 }
 
-static aom_img_fmt_t get_img_format(int subsampling_x, int subsampling_y,
-                                    int use_highbitdepth) {
+static aom_img_fmt_t get_img_format(int subsampling_x, int subsampling_y) {
   aom_img_fmt_t fmt = 0;
 
   if (subsampling_x == 0 && subsampling_y == 0)
@@ -1368,7 +1395,7 @@ static aom_img_fmt_t get_img_format(int subsampling_x, int subsampling_y,
   else if (subsampling_x == 1 && subsampling_y == 1)
     fmt = AOM_IMG_FMT_I420;
 
-  if (use_highbitdepth) fmt |= AOM_IMG_FMT_HIGHBITDEPTH;
+  fmt |= AOM_IMG_FMT_HIGHBITDEPTH;
   return fmt;
 }
 
@@ -1384,8 +1411,7 @@ static aom_codec_err_t ctrl_get_img_format(aom_codec_alg_priv_t *ctx,
       const AV1_COMMON *const cm = &frame_worker_data->pbi->common;
 
       *img_fmt = get_img_format(cm->seq_params.subsampling_x,
-                                cm->seq_params.subsampling_y,
-                                cm->seq_params.use_highbitdepth);
+                                cm->seq_params.subsampling_y);
       return AOM_CODEC_OK;
     } else {
       return AOM_CODEC_ERROR;

commit e2028df94612672cee2b31f1d8e659cd98037a0f

Fix codec control functions that image arguments
Both AV1_COPY_NEW_FRAME_IMAGE and AV1_COPY_REFERENCE take a user provided
buffer within a aom_image_t that the library is meant to fill with frame data.
Commit 857e93fa9519ec2478eacd44f32a40ac05535b23 mistakenly broke this behavior
by assuming the buffers passed were already filled and fed to the library for
internal use. This commit restores the correct behavior.

Sanity checks for bitdepth are now done in these functions instead of
av1_copy_new_frame_{dec,enc} and av1_copy_reference_{dec,enc} given that
the equal_dimensions_and_border() helper no longer does it.

av1/av1_cx_iface.c
@@ -3121,19 +3121,16 @@ static aom_codec_err_t ctrl_copy_reference(aom_codec_alg_priv_t *ctx,
   av1_ref_frame_t *const frame = va_arg(args, av1_ref_frame_t *);
 
   if (frame != NULL) {
-    aom_image_t *hbd_img = NULL;
     YV12_BUFFER_CONFIG sd;
 
     if (!(frame->img.fmt & AOM_IMG_FMT_HIGHBITDEPTH)) {
-      hbd_img = aom_img_alloc(NULL, frame->img.fmt | AOM_IMG_FMT_HIGHBITDEPTH,
-                              frame->img.w, frame->img.h, 32);
-      if (!hbd_img) return AOM_CODEC_MEM_ERROR;
-      image2yuvconfig_upshift(hbd_img, &frame->img, &sd);
-    } else {
-      image2yuvconfig(&frame->img, &sd);
+      AV1_COMMON *cm = &ctx->cpi->common;
+      aom_internal_error(&cm->error, AOM_CODEC_INVALID_PARAM,
+                         "Incorrect buffer dimensions");
+      return cm->error.error_code;
     }
+    image2yuvconfig(&frame->img, &sd);
     av1_copy_reference_enc(ctx->cpi, frame->idx, &sd);
-    aom_img_free(hbd_img);
     return AOM_CODEC_OK;
   } else {
     return AOM_CODEC_INVALID_PARAM;
@@ -3181,21 +3178,16 @@ static aom_codec_err_t ctrl_copy_new_frame_image(aom_codec_alg_priv_t *ctx,
     YV12_BUFFER_CONFIG new_frame;
 
     if (av1_get_last_show_frame(ctx->cpi, &new_frame) == 0) {
-      aom_image_t *hbd_img = NULL;
       YV12_BUFFER_CONFIG sd;
 
       if (!(new_img->fmt & AOM_IMG_FMT_HIGHBITDEPTH)) {
-        hbd_img = aom_img_alloc(NULL, new_img->fmt | AOM_IMG_FMT_HIGHBITDEPTH,
-                                new_img->w, new_img->h, 32);
-        if (!hbd_img) return AOM_CODEC_MEM_ERROR;
-        image2yuvconfig_upshift(hbd_img, new_img, &sd);
-      } else {
-        image2yuvconfig(new_img, &sd);
+        AV1_COMMON *cm = &ctx->cpi->common;
+        aom_internal_error(&cm->error, AOM_CODEC_INVALID_PARAM,
+                           "Incorrect buffer dimensions");
+        return cm->error.error_code;
       }
-      aom_codec_err_t res =
-          av1_copy_new_frame_enc(&ctx->cpi->common, &new_frame, &sd);
-      aom_img_free(hbd_img);
-      return res;
+      image2yuvconfig(new_img, &sd);
+      return av1_copy_new_frame_enc(&ctx->cpi->common, &new_frame, &sd);
     } else {
       return AOM_CODEC_ERROR;
     }

av1/av1_dx_iface.c
@@ -932,22 +932,17 @@ static aom_codec_err_t ctrl_copy_reference(aom_codec_alg_priv_t *ctx,
                                            va_list args) {
   const av1_ref_frame_t *const frame = va_arg(args, av1_ref_frame_t *);
   if (frame) {
-    aom_image_t *hbd_img = NULL;
     YV12_BUFFER_CONFIG sd;
     AVxWorker *const worker = ctx->frame_worker;
     FrameWorkerData *const frame_worker_data = (FrameWorkerData *)worker->data1;
     if (!(frame->img.fmt & AOM_IMG_FMT_HIGHBITDEPTH)) {
-      hbd_img = aom_img_alloc(NULL, frame->img.fmt | AOM_IMG_FMT_HIGHBITDEPTH,
-                              frame->img.w, frame->img.h, 32);
-      if (!hbd_img) return AOM_CODEC_MEM_ERROR;
-      image2yuvconfig_upshift(hbd_img, &frame->img, &sd);
-    } else {
-      image2yuvconfig(&frame->img, &sd);
+      AV1_COMMON *cm = &frame_worker_data->pbi->common;
+      aom_internal_error(&cm->error, AOM_CODEC_INVALID_PARAM,
+                         "Incorrect buffer dimensions");
+      return cm->error.error_code;
     }
-    aom_codec_err_t res =
-        av1_copy_reference_dec(frame_worker_data->pbi, frame->idx, &sd);
-    aom_img_free(hbd_img);
-    return res;
+    image2yuvconfig(&frame->img, &sd);
+    return av1_copy_reference_dec(frame_worker_data->pbi, frame->idx, &sd);
   } else {
     return AOM_CODEC_INVALID_PARAM;
   }
@@ -997,20 +992,16 @@ static aom_codec_err_t ctrl_copy_new_frame_image(aom_codec_alg_priv_t *ctx,
     FrameWorkerData *const frame_worker_data = (FrameWorkerData *)worker->data1;
 
     if (av1_get_frame_to_show(frame_worker_data->pbi, &new_frame) == 0) {
-      aom_image_t *hbd_img = NULL;
       YV12_BUFFER_CONFIG sd;
       if (!(img->fmt & AOM_IMG_FMT_HIGHBITDEPTH)) {
-        hbd_img = aom_img_alloc(NULL, img->fmt | AOM_IMG_FMT_HIGHBITDEPTH,
-                                img->w, img->h, 32);
-        if (!hbd_img) return AOM_CODEC_MEM_ERROR;
-        image2yuvconfig_upshift(hbd_img, img, &sd);
-      } else {
-        image2yuvconfig(img, &sd);
+        AV1_COMMON *cm = &frame_worker_data->pbi->common;
+        aom_internal_error(&cm->error, AOM_CODEC_INVALID_PARAM,
+                           "Incorrect buffer dimensions");
+        return cm->error.error_code;
       }
-      aom_codec_err_t res = av1_copy_new_frame_dec(
-          &frame_worker_data->pbi->common, &new_frame, &sd);
-      aom_img_free(hbd_img);
-      return res;
+      image2yuvconfig(img, &sd);
+      return av1_copy_new_frame_dec(&frame_worker_data->pbi->common, &new_frame,
+                                    &sd);
     } else {
       return AOM_CODEC_ERROR;
     }