b6c9e567a720c912e8ee33971f8b9557e30b7264
Urvang Joshi <urvang@google.com>
2022-08-03 08:46:50 -0700

    precise condition and brackets

--- av1/common/warped_motion.h
+++ av1/common/warped_motion.h
@@ -111,9 +111,12 @@ static INLINE int16_t resolve_divisor_32_CfL(int32_t N, int32_t D,
 
     const int shift_add = shift_d - shift_n - shift;
 
-    if (shift_add <= 2) {
-      ret =
-          div_lut[f_d] * f_n >> (DIV_LUT_PREC_BITS + DIV_LUT_BITS + shift_add);
+    // The maximum value of `div_lut[f_d] * f_n` is
+    // `1 << (DIV_LUT_PREC_BITS + DIV_LUT_BITS + 1)`
+    // Hence `shift_add`below is constrained to be <= 1.
+    if (shift_add <= 1) {
+      ret = (div_lut[f_d] * f_n) >>
+            (DIV_LUT_PREC_BITS + DIV_LUT_BITS + shift_add);
     } else {
       ret = 0;
     }
