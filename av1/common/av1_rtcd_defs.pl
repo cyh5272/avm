d5239625e9a5c8d4905b262c2738fc0f7f9c9242
Arun Negi <arun.negi@ittiam.com>
2022-08-29 18:21:45 +0000

    Add AVX2 intrinsic for wiener non separable dual filtering
    This CL adds AVX2 optimization for the function  
    av1_convolve_symmetric_dual_subtract_center_highbd_c(),  
    which corresponds to CONFIG_WIENER_NONSEP_CROSS_FILT  
    loop restoration filter.  
    
    Observed module level scaling of 10.5x w.r.t. C for block  
    size 4x4.

--- av1/common/av1_rtcd_defs.pl
+++ av1/common/av1_rtcd_defs.pl
@@ -93,6 +93,7 @@ add_proto qw/void av1_convolve_symmetric_dual_highbd/, "const uint16_t *dgd, int
 add_proto qw/void av1_convolve_symmetric_subtract_center_highbd/, "const uint16_t *dgd, int stride, const NonsepFilterConfig *filter_config, const int16_t *filter, uint16_t *dst, int dst_stride, int bit_depth, int block_row_begin, int block_row_end, int block_col_begin, int block_col_end";
 specialize qw/av1_convolve_symmetric_subtract_center_highbd avx2/;
 add_proto qw/void av1_convolve_symmetric_dual_subtract_center_highbd/, "const uint16_t *dgd, int dgd_stride, const uint16_t *dgd_dual, int dgd_dual_stride, const NonsepFilterConfig *filter_config, const int16_t *filter, uint16_t *dst, int dst_stride, int bit_depth, int block_row_begin, int block_row_end, int block_col_begin, int block_col_end";
+specialize qw/av1_convolve_symmetric_dual_subtract_center_highbd avx2/;
 
 # directional intra predictor functions
 add_proto qw/void av1_dr_prediction_z1/, "uint8_t *dst, ptrdiff_t stride, int bw, int bh, const uint8_t *above, const uint8_t *left, int upsample_above, int dx, int dy, int mrl_index";
