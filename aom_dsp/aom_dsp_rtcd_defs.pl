2c1386f661f329fbb020582acb65ebdbc4014d65
chiyotsai <chiyotsai@google.com>
2023-04-19 16:08:01 -0700

    Add SSE4_1 obmc variance for BLOCK_256

--- aom_dsp/aom_dsp_rtcd_defs.pl
+++ aom_dsp/aom_dsp_rtcd_defs.pl
@@ -589,9 +589,7 @@ if (aom_config("CONFIG_AV1_ENCODER") eq "yes") {
       ($w, $h) = @$_;
       add_proto qw/unsigned int/, "aom_highbd${bd}obmc_variance${w}x${h}", "const uint16_t *pre, int pre_stride, const int32_t *wsrc, const int32_t *mask, unsigned int *sse";
       add_proto qw/unsigned int/, "aom_highbd${bd}obmc_sub_pixel_variance${w}x${h}", "const uint16_t *pre, int pre_stride, int xoffset, int yoffset, const int32_t *wsrc, const int32_t *mask, unsigned int *sse";
-      if ($w != 256 && $h != 256) {
-        specialize "aom_highbd${bd}obmc_variance${w}x${h}", qw/sse4_1/;
-      }
+      specialize "aom_highbd${bd}obmc_variance${w}x${h}", qw/sse4_1/;
     }
   }
 
