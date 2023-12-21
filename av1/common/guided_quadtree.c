72f874e42525cdbbcc25d3ea7f9c421e92c73eb8
Urvang Joshi <urvang@google.com>
2023-12-21 22:35:03 +0000

    Use 3k flops/pixel models for superres
    This matches the models used for same-resolution restoration.
    
    Note: earlier, superresolution was using 1.2k flops/pixel models.

--- av1/common/guided_quadtree.c
+++ av1/common/guided_quadtree.c
@@ -172,38 +172,38 @@ int qp90_quadtree_model_quantSet_inter[] = { 116948, 143520, -1, 2 };
 
 #if CONFIG_EXT_SUPERRES
 // Superres guided conv unet intra.
-int sr2by1ai_1_quantset[] = { 982633, 1534950, -6, -8 };
-int sr2by1ai_2_quantset[] = { 14303, 3648, -9, -6 };
-int sr2by1ai_3_quantset[] = { 415158, 458378, -7, -5 };
+int sr2by1ai_1_quantset[] = { 17235, 16969, -10, -10 };
+int sr2by1ai_2_quantset[] = { 76705024, 109138704, -10, -7 };
+int sr2by1ai_3_quantset[] = { 335, 464, -4, -8 };
 
-int sr3by2ai_1_quantset[] = { 117, 133, -5, -3 };
-int sr3by2ai_2_quantset[] = { 1157, 750, -11, -7 };
-int sr3by2ai_3_quantset[] = { 20383, 45842, -6, -9 };
+int sr3by2ai_1_quantset[] = { 385035, 450967, -8, -10 };
+int sr3by2ai_2_quantset[] = { 54186, 53143, 2, -13 };
+int sr3by2ai_3_quantset[] = { 608, 795, -5, -8 };
 
-int sr5by4ai_1_quantset[] = { 10532, 7396, -8, -18 };
+int sr5by4ai_1_quantset[] = { 57611, 154741, -2, 2 };
 int sr5by4ai_2_quantset[] = { 166, 503, -12, -1 };
 int sr5by4ai_3_quantset[] = { 11, 4, -11, -9 };
 
-int sr7by4ai_1_quantset[] = { 2631408, 4529404, -8, -8 };
+int sr7by4ai_1_quantset[] = { 2631403, 4529410, -8, -8 };
 int sr7by4ai_2_quantset[] = { 28290, 12216, 1, -3 };
 int sr7by4ai_3_quantset[] = { 11, 9, -7, -8 };
 
 // Superres guided conv unet inter.
-int sr2by1ra_1_quantset[] = { 1149534, 1617706, -6, -7 };
-int sr2by1ra_2_quantset[] = { 23807, 4335, -8, -6 };
-int sr2by1ra_3_quantset[] = { 242788, 346460, -7, -6 };
+int sr2by1ra_1_quantset[] = { 15680, 12890, -10, -9 };
+int sr2by1ra_2_quantset[] = { 85696504, 103679088, -8, -9 };
+int sr2by1ra_3_quantset[] = { 2067139, 493382, -6, -6 };
 
-int sr3by2ra_1_quantset[] = { 124, 143, -6, -2 };
-int sr3by2ra_2_quantset[] = { 10622, 14766, -7, -9 };
-int sr3by2ra_3_quantset[] = { 10871, 23315, -5, -5 };
+int sr3by2ra_1_quantset[] = { 651704, 364241, -9, -9 };
+int sr3by2ra_2_quantset[] = { 89884, 88521, 3, -12 };
+int sr3by2ra_3_quantset[] = { 3133894, 9945744, -7, -11 };
 
-int sr5by4ra_1_quantset[] = { 8492, 7778, -8, -18 };
-int sr5by4ra_2_quantset[] = { 221, 819, -13, 1 };
-int sr5by4ra_3_quantset[] = { 11, 7, -11, -10 };
+int sr5by4ra_1_quantset[] = { 40478, 106889, -3, 3 };
+int sr5by4ra_2_quantset[] = { 424139, 223163, 6, 0 };
+int sr5by4ra_3_quantset[] = { 164313, 57994, -5, -7 };
 
-int sr7by4ra_1_quantset[] = { 2461636, 2223173, -9, -8 };
-int sr7by4ra_2_quantset[] = { 37154, 12358, -1, -3 };
-int sr7by4ra_3_quantset[] = { 179, 72, -7, -3 };
+int sr7by4ra_1_quantset[] = { 8734122, 6400229, -6, -7 };
+int sr7by4ra_2_quantset[] = { 295, 36, -3, -9 };
+int sr7by4ra_3_quantset[] = { 21017, 33976, -13, -6 };
 #endif  // CONFIG_EXT_SUPERRES
 
 int *get_quadparm_from_qindex(int qindex, int superres_denom, int is_intra_only,
