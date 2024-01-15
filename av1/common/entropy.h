f4ab0f961bdc4cd396cf205e9c0c58edaccfbbae
Rubén Aciego <raciego@netflix.com>
2024-01-15 14:16:19 -0800

    1 lr

--- av1/common/entropy.h
+++ av1/common/entropy.h
@@ -97,7 +97,7 @@ extern "C" {
 #define NUM_BASE_LEVELS 2
 
 #define BR_CDF_SIZE (4)
-#define COEFF_BASE_RANGE (4 * (BR_CDF_SIZE - 1))
+#define COEFF_BASE_RANGE (1 * (BR_CDF_SIZE - 1))
 
 #define COEFF_CONTEXT_BITS 3
 #define COEFF_CONTEXT_MASK ((1 << COEFF_CONTEXT_BITS) - 1)
